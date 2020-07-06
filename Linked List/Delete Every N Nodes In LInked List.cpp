f4f9588d37b87479e5cc5407aba4ce2d1520fdbf
Ajay kumar shukla <sajay6366@gmail.com>
2020-07-06 16:36:13 +0530

    Delete Every nth Elemnt In A Linked List

--- Linked List/Delete Every N Nodes In LInked List.cpp
+++ Linked List/Delete Every N Nodes In LInked List.cpp
@@ -1,3 +1,35 @@
+/*
+Delete every N nodes
+
+Given a linked list and two integers M and N. Traverse the linked list such that you retain
+M nodes then delete next N nodes, continue the same until end of the linked list. That is,
+in the given linked list you need to delete N nodes after every M nodes.
+
+Input format :
+
+Line 1 : Linked list elements (separated by space and terminated by -1)
+
+Line 2 : M
+
+Line 3 : N
+
+Sample Input 1 :
+1 2 3 4 5 6 7 8 -1
+2
+2
+
+Sample Output 1 :
+1 2 5 6
+
+Sample Input 2 :
+1 2 3 4 5 6 7 8 -1
+2
+3
+
+Sample Output 2 :
+1 2 6 7
+*/
+
 #include<iostream>
 using namespace std;
 
@@ -49,6 +81,10 @@ Node* skipMdeleteN(Node *head,int m,int n){
       t1 = t1->next;
       c1++;
     }
+    if(t1==NULL)
+      {
+          return head;
+      }
     t2 = t1->next;
     while(c2!= n && t2!= NULL){
       t2 = t2->next;
