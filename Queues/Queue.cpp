e9a00b06117f707595b797b0badbf7baec95372a
Ajay kumar shukla <sajay6366@gmail.com>
2020-07-11 09:50:54 +0530

    A Simple Program is created for checking the functionality of Queues

---
+++ Queues/Queue.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+#include"QueueUsingLinkedList.h"
+
+int main()
+{
+  QueueUsingLL<int> q;
+  q.enqueue(10);
+  q.enqueue(20);
+  q.enqueue(30);
+  q.enqueue(40);
+  q.enqueue(50);
+  q.enqueue(60);
+  q.enqueue(70);
+  cout <<q.getSize()<<endl;
+  cout<<q.dequeue()<<endl;
+  cout<<q.dequeue()<<endl;
+  cout <<q.front()<<endl;
+  cout <<q.getSize()<<endl;
+  cout <<q.isEmpty()<<endl;
+  cout<<q.dequeue()<<endl;
+  cout<<q.dequeue()<<endl;
+  cout<<q.dequeue()<<endl;
+  cout <<q.front()<<endl;
+  cout <<q.getSize()<<endl;
+  cout <<q.isEmpty()<<endl;
+  cout<<q.dequeue()<<endl;
+  cout<<q.dequeue()<<endl;
+}
