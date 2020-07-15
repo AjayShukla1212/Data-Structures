bdfee950e697a8f4199e23e4aa56a0876e8dc7a6
Ajay kumar shukla <sajay6366@gmail.com>
2020-07-15 12:27:43 +0530

    Added The Problem of the program in a comment

--- Linked List/kReverse.cpp
+++ Linked List/kReverse.cpp
@@ -1,3 +1,25 @@
+/*
+kReverse
+
+Implement kReverse( int k ) in a linked list i.e. you need to reverse first K elements
+ then reverse next k elements and join the linked list and so on.
+Indexing starts from 0. If less than k elements left in the last, you need to reverse
+them as well. If k is greater than length of LL, reverse the complete LL.
+You don't need to print the elements, just return the head of updated LL.
+
+Input format :
+
+Line 1 : Linked list elements (separated by space and terminated by -1)
+
+Line 2 : k
+
+Sample Input 1 :
+1 2 3 4 5 6 7 8 9 10 -1
+4
+Sample Output 1 :
+4 3 2 1 8 7 6 5 10 9
+
+*/
 #include<iostream>
 using namespace std;
 
