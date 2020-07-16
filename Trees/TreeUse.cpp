776a134fa6fde92c59c2d9b8328f96dcabb5e731
Ajay kumar shukla <sajay6366@gmail.com>
2020-07-16 20:18:13 +0530

    Created A New Folder For Trees

---
+++ Trees/TreeUse.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+#include "Tree_class.h"
+
+TreeNode<int>* takeInput(){
+  int rootData;
+  cout <<"Enter Data : ";
+  cin>>rootData;
+  TreeNode<int>* root = new TreeNode<int> (rootData);
+  int n;
+  cout <<"Enter The Children Of "<<rootData;
+  cin >>n;
+  for(int i=0;i<n;i++)
+  {
+    TreeNode<int>* child = takeInput();
+    root->children.push_back(child);
+  }
+  return root;
+}
+
+void printTree(TreeNode<int>* root)
+{
+  if(root==NULL)   //Edge Case
+  {
+    return ;
+  }
+  cout <<root->data <<":";
+  for(int i=0;i<root->children.size();i++)
+  {
+    cout <<root->children[i]->data<<",";
+  }
+  cout <<endl;
+  for (int i=0;i<root->children.size();i++)
+  {
+    printTree(root->children[i]);
+  }
+
+}
+
+int main()
+{
+  TreeNode<int> *root = takeInput();
+
+  printTree(root);
+}
