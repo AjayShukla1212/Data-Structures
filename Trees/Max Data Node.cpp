6c84ed86ed4ca5081806a301ccb3c17a84dd9193
Ajay kumar shukla <sajay6366@gmail.com>
2020-07-18 21:10:21 +0530

    Program To Get The Max Data In A Tree

---
+++ Trees/Max Data Node.cpp
@@ -0,0 +1,81 @@
+#include<queue>
+#include<iostream>
+using namespace std;
+#include"Tree_class.h"
+
+TreeNode<int>* TakeInput(){
+  int rootData;
+  cout <<"Enter The Root Data : ";
+  cin >>rootData;
+  TreeNode<int>* root = new TreeNode<int> (rootData);
+  queue<TreeNode<int>*> pendingNode;
+  pendingNode.push(root);
+  while(!pendingNode.empty())
+  {
+    TreeNode<int>* front = pendingNode.front();
+    pendingNode.pop();
+    int numChild;
+    cout <<"Enter The Number Of Children Of "<<front->data<<endl;
+    cin >>numChild;
+    for(int i=0;i<numChild;i++)
+    {
+      int childData;
+      cout <<"Enter The "<<i<<"Th Child Of "<<front->data<<endl;
+      cin >>childData;
+      TreeNode<int>* child = new TreeNode<int> (childData);
+      front->children.push_back(child);
+      pendingNode.push(child);
+    }
+  }
+  return root;
+}
+void print(TreeNode<int>* root)
+{
+  queue<TreeNode<int>*> pendingNodes;
+pendingNodes.push(root);
+while(!pendingNodes.empty())
+{
+  TreeNode<int>* front = pendingNodes.front();
+  pendingNodes.pop();
+  cout <<front->data<<":";
+
+
+  for(int i=0;i<front->children.size();i++)
+  {
+    if(i==front->children.size()-1){
+  cout<<front->children[i]->data;
+    }
+
+else{
+       cout<<front->children[i]->data<<",";
+    }
+    pendingNodes.push(front->children[i]);
+  }
+  cout <<endl;
+}
+}
+
+TreeNode<int>* maxDataNode(TreeNode<int>* root) {
+  if(root==NULL)
+  {
+    return 0;
+  }
+  TreeNode<int>* max = NULL;
+  for(int i=0;i<root->children.size();i++)
+  {
+    TreeNode<int>* ans = maxDataNode(root->children[i])
+    if(max->data < ans->data)
+    {
+      max = ans;
+    }
+  }
+
+  return max;
+}
+
+int main()
+{
+  TreeNode<int>* root  = TakeInput();
+  print(root);
+  cout <<"The Max Among All Nodes Are : "<<maxDataNode(root)->data;
+}
