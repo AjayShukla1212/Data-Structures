671e312a5e3ac2a871c7d157748edd59150332ce
Ajay kumar shukla <sajay6366@gmail.com>
2020-08-24 02:28:37 +0530

    A Class Program for Min Priority Queue / Min Heap is Built

---
+++ Priority Queues/Min Class Check.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+#include <climits>
+#include "Priority Queue Class.h"
+
+int main() {
+    PriorityQueue pq;
+    int choice;
+    cin >> choice;
+    while(choice != -1) {
+        switch(choice) {
+            case 1 : // insert
+                int element;
+                cin >> element;
+                pq.insert(element);
+                break;
+            case 2 : // getMin
+                cout << pq.getMin() << endl;
+                break;
+            case 3 : // removeMax
+                cout << pq.removeMin() << endl;
+                break;
+            case 4 : // size
+                cout << pq.getSize() << endl;
+                break;
+            case 5 : // isEmpty
+                if(pq.isEmpty()) {
+                    cout << "true" << endl;
+                }
+                else {
+                    cout << "false" << endl;
+                }
+            default :
+                return 0;
+        }
+        cin >> choice;
+    }
+}
---
+++ Priority Queues/Priority Queue Class.h
@@ -0,0 +1,126 @@
+#include <vector>
+
+/*******************
+ * Main function -
+*
+int main() {
+    PriorityQueue pq;
+    int choice;
+    cin >> choice;
+    while(choice != -1) {
+        switch(choice) {
+            case 1 : // insert
+                int element;
+                cin >> element;
+                pq.insert(element);
+                break;
+            case 2 : // getMin
+                cout << pq.getMin() << endl;
+                break;
+            case 3 : // removeMin
+                cout << pq.removeMin() << endl;
+                break;
+            case 4 : // size
+                cout << pq.getSize() << endl;
+                break;
+            case 5 : // isEmpty
+                if(pq.isEmpty()) {
+                    cout << "true" << endl;
+                }
+                else {
+                    cout << "false" << endl;
+                }
+            default :
+                return 0;
+        }
+        cin >> choice;
+    }
+}
+**************************/
+
+
+class PriorityQueue {
+    vector<int> pq;
+
+    public :
+
+    PriorityQueue() {
+
+    }
+
+    bool isEmpty() {
+        return pq.size() == 0;
+    }
+
+    // Return the size of priorityQueue - no of elements present
+    int getSize() {
+        return pq.size();
+    }
+
+    int getMin() {
+        if(isEmpty()) {
+            return 0;		// Priority Queue is empty
+        }
+        return pq[0];
+    }
+
+    void insert(int element) {
+        pq.push_back(element);
+
+        int childIndex = pq.size() - 1;
+
+        while(childIndex > 0) {
+            int parentIndex = (childIndex - 1) / 2;
+
+            if(pq[childIndex] < pq[parentIndex]) {
+                int temp = pq[childIndex];
+                pq[childIndex] = pq[parentIndex];
+                pq[parentIndex] = temp;
+            }
+            else {
+                break;
+            }
+            childIndex = parentIndex;
+        }
+
+    }
+
+    int removeMin() {
+        // Complete this function
+        int ans = pq[0];
+        pq[0] = pq[pq.size()-1];
+        pq.pop_back();
+
+        int parentIndex = 0;
+        int LCI = 2 * parentIndex + 1;
+        int RCI = 2 * parentIndex + 2;
+        while(LCI<pq.size()){
+
+            int minIndex = parentIndex;
+
+            if(pq[LCI]<pq[minIndex]){
+                minIndex = LCI;
+            }
+
+            if(RCI < pq.size() && pq[RCI] < pq[minIndex]){
+                minIndex = RCI;
+            }
+
+            if(minIndex == parentIndex){
+                break;
+            }
+            int temp = pq[minIndex];
+            pq[minIndex] = pq[parentIndex];
+            pq[parentIndex] = temp;
+
+            parentIndex = minIndex;
+            LCI = 2*parentIndex+1;
+        	RCI = 2*parentIndex+2;
+        }
+
+
+        return ans;
+    }
+
+
+};
